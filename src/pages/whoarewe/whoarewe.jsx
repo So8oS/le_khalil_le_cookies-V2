4298d40cf5e69e0ff69386574fbdfbeaf7ac0845
So8oS <ahmadothman033@gmail.com>
2022-06-19 22:27:09 +0300

    media query update

--- src/pages/whoarewe/whoarewe.jsx
+++ src/pages/whoarewe/whoarewe.jsx
@@ -7,7 +7,7 @@ export const Whoarewe = () => {
     <div className="w-container">
       <div className="box">
         <div className="img-box">
-          <img src={Image} alt="khalilpic" />
+          <img className="whoarewe-img" src={Image} alt="khalilpic" />
         </div>
         <h1 className="title">khalil Selyan</h1>
         <p className="p">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
